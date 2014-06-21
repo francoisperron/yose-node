2fd6fe104914f93f410089d2d8b533cf52c67b6f
Francois Perron <fperron@gmail.com>
2014-06-20 22:44:12 -0400

    primeFactors string guard

--- primefactors/power.of.two.spec.js
+++ primefactors/power.of.two.spec.js
@@ -31,4 +31,13 @@ describe('The power of two endpoint', function () {
             done();
         });
     });
+
+    it('returns an error when received number is not a number', function (done) {
+        request('http://localhost:5000/primeFactors?number=banane', function (error, response, body) {
+            expect(response.statusCode).toEqual(200);
+            expect(response.headers['content-type']).toEqual('application/json');
+            expect(JSON.parse(response.body).error).toEqual("not a number");
+            done();
+        });
+    });
 });
--- primefactors/prime.factors.js
+++ primefactors/prime.factors.js
@@ -6,12 +6,28 @@ function PrimeFactors() {
 PrimeFactors.prototype.answer = function (request, response) {
     var query = url.parse(request.url, true).query;
     var number = parseInt(query.number);
+
+    if (isNaN(number)) {
+        answerError(number, response);
+    }
+    else {
+        answerDecomposition(number, response);
+    }
+};
+
+function answerError(number, response) {
+    response.setHeader('Content-Type', 'application/json');
+    response.write(JSON.stringify({number: number, error: "not a number"}));
+    response.end();
+}
+
+function answerDecomposition(number, response) {
     var decomposition = decompose(number);
 
     response.setHeader('Content-Type', 'application/json');
     response.write(JSON.stringify({number: number, decomposition: decomposition}));
     response.end();
-};
+}
 
 function decompose(number) {
     var decomposition = [];
@@ -21,4 +37,5 @@ function decompose(number) {
     }
     return decomposition;
 }
+
 module.exports = PrimeFactors;
